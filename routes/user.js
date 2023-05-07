dcd392d07b75ebb90693456c933f7737a3aa65f0
pal-ef <soft.pal.ef@gmail.com>
2023-05-07 16:45:26 -0600

    crendentials verify

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const pool = require('../db');
+const pool = require("../db");
 
-// Get all users 
-router.get('/', async(req, res) => {
-    const [result] = await pool.query('SELECT * FROM users');
+// Verify credentials (insecure)
+router.post("/credentials", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const [result] = await pool.query(
+        "SELECT * FROM users WHERE username = ? AND password = ?",
+        [username, password]
+    );
+    res.json(result[0]);
+});
+
+// Get all users
+router.get("/", async (req, res) => {
+    const [result] = await pool.query("SELECT * FROM users");
     res.json(result);
 });
 
 // Create new user
 router.post("/", async (req, res) => {
-    const {username, name, phone, email, password} = req.body;
+    const { username, name, phone, email, password } = req.body;
     const [result] = await pool.query(
         `INSERT INTO users (username, name, phone, email, password) VALUES(?,?,?,?,?)`,
         [username, name, phone, email, password]
@@ -20,39 +32,50 @@ router.post("/", async (req, res) => {
 
 // Get user data
 router.get("/:id", async (req, res) => {
-    const [result] = await pool.query('SELECT * FROM users WHERE id = ?', [req.params.id]);
+    const [result] = await pool.query("SELECT * FROM users WHERE id = ?", [
+        req.params.id,
+    ]);
     res.json(result[0]);
 });
 
 // Update user
 router.put("/:id", async (req, res) => {
     // Verify if such user exists
-    const [user] = await pool.query('SELECT * FROM users WHERE id = ?', [req.params.id]);
+    const [user] = await pool.query("SELECT * FROM users WHERE id = ?", [
+        req.params.id,
+    ]);
 
     // If there's such user update the data
-    if(user[0]) {
+    if (user[0]) {
         const firstName = req.body.firstName || user[0].firstName;
         const lastName = req.body.firstName || user[0].firstName;
         const phone = req.body.firstName || user[0].firstName;
 
-        const [result] = await pool.query('UPDATE users SET firstName = ?, lastName = ?, phone = ? WHERE ID = ?', [firstName, lastName, phone, req.params.id]);
+        const [result] = await pool.query(
+            "UPDATE users SET firstName = ?, lastName = ?, phone = ? WHERE ID = ?",
+            [firstName, lastName, phone, req.params.id]
+        );
         res.json(result[0]);
     } else {
-        res.status(303).send('User was not found');
+        res.status(303).send("User was not found");
     }
 });
 
 // Delete user
 router.delete("/:id", async (req, res) => {
     // Verify if such user exists
-    const [user] = await pool.query('SELECT * FROM users WHERE id = ?', [req.params.id]);
+    const [user] = await pool.query("SELECT * FROM users WHERE id = ?", [
+        req.params.id,
+    ]);
 
     // If there's such user delete it
-    if(user[0]) {
-        const [result] = await pool.query('DELETE FROM users WHERE ID = ?', [req.params.id]);
+    if (user[0]) {
+        const [result] = await pool.query("DELETE FROM users WHERE ID = ?", [
+            req.params.id,
+        ]);
         res.json(result[0]);
     } else {
-        res.status(303).send('User does not exists');
+        res.status(303).send("User does not exists");
     }
 });
 
